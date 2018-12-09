2de4591b724bb8c64004efd16798e92e371e779f
turmanauli <bsovevo@gmail.com>
2018-12-09 16:49:36 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#function to get the length of the longest sub-string
+def lengthOfLongestSubstring(s):
+        cUn = ""
+        lUn = ""
+        for x in s:
+            if x in cUn:
+                cUn = cUn.split(x, 2)[1]+x
+            else:
+                cUn += x
+                if len(cUn)>len(lUn):
+                    lUn = cUn
+        return len(lUn)
+        
+#function to generate longest sub-string        
+def longestSubstring(s):
+    cUn = ""
+    lUn = ""
+    for x in s:
+        if x in cUn:
+            cUn = cUn.split(x, 2)[1]+x
+        else:
+            cUn += x
+            if len(cUn)>len(lUn):
+                lUn = cUn
+    return len(lUn)
