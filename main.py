acfd6e5b803088cfff7f5a6d588a7b59e1d12b30
turmanauli <bsovevo@gmail.com>
2018-12-09 16:53:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ def lengthOfLongestSubstring(s):
         cUn = ""
         lUn = ""
         for x in s:
-            if x in cUn:
-                cUn = cUn.split(x, 2)[1]+x
-            else:
-                cUn += x
+                if x in cUn:
+                        cUn = cUn.split(x, 2)[1]+x
+                else:
+                        cUn += x
                 if len(cUn)>len(lUn):
-                    lUn = cUn
+                        lUn = cUn
         return len(lUn)
         
 #function to generate longest sub-string        
 def longestSubstring(s):
-    cUn = ""
-    lUn = ""
-    for x in s:
-        if x in cUn:
-            cUn = cUn.split(x, 2)[1]+x
-        else:
-            cUn += x
-            if len(cUn)>len(lUn):
-                lUn = cUn
-    return len(lUn)
+        cUn = ""
+        lUn = ""
+        for x in s:
+                if x in cUn:
+                        cUn = cUn.split(x, 2)[1]+x
+                else:
+                        cUn += x
+                if len(cUn)>len(lUn):
+                        lUn = cUn
+        return len(lUn)
